6dd5e6f1037fe97b87cfa89b3bff6ea497a1f162
luckyluke5 <l.maron@t-online.de>
2021-02-23 11:44:30 +0100

    Model Update

--- habits_trainer/models.py
+++ habits_trainer/models.py
@@ -1,8 +1,11 @@
 import statistics
-from datetime import timedelta, datetime
+from datetime import timedelta, datetime, timezone
+# from time import
 from typing import List
 
 from django.db import models
+
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -12,19 +15,20 @@ class Task(models.Model):
     name = models.CharField(max_length=50)
     interval = models.FloatField(default=7.0)
     targetInterval = models.FloatField(default=7.0, editable=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name.__str__()
 
     def mean_interval(self, to_date=datetime.now()):
-        ordered_task_done = self.taskdone_set.filter(doneDate__lte=to_date).order_by('doneDate')
+        ordered_task_done = self.taskdone_set.filter(done_date__lte=to_date).order_by('done_date')
 
         last_date = None
 
         intervals: List[timedelta] = []
 
         for taskDone in ordered_task_done.all():
-            date = taskDone.doneDate
+            date = taskDone.done_date
             if last_date:
                 intervals.append(date - last_date)
 
@@ -32,10 +36,12 @@ class Task(models.Model):
 
         if intervals:
 
-            mean_in_days = statistics.mean([interval.days for interval in intervals])
+            mean_in_seconds = statistics.mean([interval.total_seconds() for interval in intervals])
+
+            # return sum(intervals)/len(intervals)
             # [interval.days for interval in intervals]
-            print(mean_in_days)
-            return timedelta(days=mean_in_days)
+            print(mean_in_seconds)
+            return timedelta(seconds=mean_in_seconds)
 
         else:
             return None
@@ -47,11 +53,26 @@ class Task(models.Model):
         elif self.taskfeedback_set.latest("date").feedback == TaskFeedback.Behavior.LATER:
             self.interval *= 2
 
+
     def predict_next_date(self):
-        return self.taskdone_set.latest("doneDate").predict_next_date()
+        try:
+            next_date = self.taskdone_set.latest("done_date").predict_next_date()
+
+        except:
+            return datetime.now(timezone.utc)
+
+        return next_date
+
+        # if next_date:
+        #    return next_date
+        # else:
+        #    datetime.now()
+
+    def last_done_task_before(self, to_date=datetime.now()):
+        return self.taskdone_set.filter(done_date__lt=to_date).latest('done_date')
 
     # def usual_delay(self):
-    #     ordered_task_done = self.taskdone_set.order_by('doneDate')
+    #     ordered_task_done = self.taskdone_set.order_by('done_date')
     #
     #     last_predicted_next_date = None
     #
@@ -60,7 +81,7 @@ class Task(models.Model):
     #     for taskDone in ordered_task_done.all():
     #         predicted_next_date = taskDone.predicted_next_date()
     #         if last_predicted_next_date:
-    #             intervals.append(taskDone.doneDate - last_predicted_next_date)
+    #             intervals.append(taskDone.done_date - last_predicted_next_date)
     #
     #         last_predicted_next_date = predicted_next_date
     #
@@ -101,15 +122,27 @@ class TaskFeedback(models.Model):
 
 class TaskDone(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    doneDate = models.DateTimeField(null=True)
+    done_date = models.DateTimeField(null=True)
 
     # predicted_next_date = models.DateTimeField(null=True)
 
     # actual_interval = models.FloatField()
 
     def __str__(self):
-        return " ".join([self.task.name.__str__(), self.doneDate.__str__()])
+        return " ".join([self.task.name.__str__(), self.done_date.__str__()])
 
-    def predict_next_date(self):
-        if self.task.mean_interval(self.doneDate):
-            return self.doneDate + self.task.mean_interval(self.doneDate)
+    def predict_next_date(self) -> datetime:
+        if self.task.mean_interval(self.done_date):
+            return self.done_date + self.task.mean_interval(self.done_date)
+
+    def delay(self):
+        prediction_at_last_done_date = self.task.last_done_task_before(self.done_date).predict_next_date()
+        if prediction_at_last_done_date:
+
+            return self.done_date - prediction_at_last_done_date
+        else:
+            return None
+
+    def mean_interval(self):
+
+        return self.task.mean_interval(self.done_date)
